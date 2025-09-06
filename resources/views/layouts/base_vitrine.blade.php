e013dab971ca3993c1c9d1752a55ae43235da825
Ethandudu <41381482+ethandudu@users.noreply.github.com>
2025-09-07 00:48:05 +0200

    fixed tracking

--- resources/views/layouts/base_vitrine.blade.php
+++ resources/views/layouts/base_vitrine.blade.php
@@ -17,11 +17,11 @@
     <meta property="og:site" content="https://eloquencia.org">
     <meta property="og:title" content="Accueil">
     <meta property="og:description" content="Eloquéncia est une association loi 1901 visant à promouvoir l'éloquence et l'art oratoire">
-    <script defer type="text/javascript" src="js/klaro-js/config.js"></script>
+    <script async="" src="https://analytics.eloquencia.org/matomo.js"></script>
+    <script defer type="text/javascript" src="js/klaro-config.js"></script>
     <script defer type="text/javascript" src="https://cdn.kiprotect.com/klaro/v0.7.22/klaro.js"></script>
     <link rel="stylesheet" href="https://cdn.kiprotect.com/klaro/v0.7.22/klaro.min.css" />
-    <noscript><img referrerpolicy="no-referrer-when-downgrade" src="https://analytics.eloquencia.org/matomo.php?idsite=1&amp;rec=1" style="border:0" alt="" /></noscript>
-
+    <noscript><p><img referrerpolicy="no-referrer-when-downgrade" data-src="https://analytics.eloquencia.org/matomo.php?idsite=1&amp;rec=1" style="border:0;" alt="" data-type="image" data-name="matomo"/></p></noscript>
   <!-- Bootstrap CDN -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <!-- Icons8 -->
@@ -95,5 +95,6 @@
 
 
 </script>
+  <div id="klaro" style="--button-text-color: #fff; --dark1: #fafafa; --dark2: #777; --dark3: #555; --light1: #444; --light2: #666; --light3: #111; --green3: #f00; --notice-top: 20px; --notice-bottom: auto; --notice-left: 20px; --notice-right: auto; --notice-max-width: calc(100vw - 60px); --notice-position: fixed;"><div lang="fr" class="klaro"><div id="cookieScreen" class="cookie-modal"><div class="cm-bg"></div><div class="cm-modal cm-klaro"><div class="cm-header"><h1 class="title"><span><span>Services que nous souhaitons utiliser</span></span></h1><p><span><span>Vous pouvez ici évaluer et personnaliser les services que nous aimerions utiliser sur ce site. C'est vous qui décidez ! Activez ou désactivez les services comme bon vous semble.</span><span> </span><span>Pour en savoir plus, veuillez lire notre </span><a href="/legal" target="_blank" rel="noopener">politique de confidentialité</a><span>.</span></span></p></div><div class="cm-body"><ul class="cm-purposes"><li class="cm-purpose"><input id="purpose-item-analytics" aria-labelledby="purpose-item-analytics-title" aria-describedby="purpose-item-analytics-description" type="checkbox" class="cm-list-input"><label for="purpose-item-analytics" class="cm-list-label"><span id="purpose-item-analytics-title" class="cm-list-title">Analytics</span><span class="cm-switch"><div class="slider round active"></div></span></label><div id="purpose-item-analytics-description"></div><div class="cm-services"><div class="cm-caret"><a href="#" aria-haspopup="true" aria-expanded="false" tabindex="0"><span>↓</span> 1 service</a></div><ul class="cm-content"><li class="cm-service"><div><input id="service-item-matomo" aria-labelledby="service-item-matomo-title" aria-describedby="service-item-matomo-description" tabindex="-1" type="checkbox" class="cm-list-input"><label for="service-item-matomo" class="cm-list-label"><span id="service-item-matomo-title" class="cm-list-title">Matomo</span><span title="Ce service est chargé par défaut (mais vous pouvez le désactiver)" class="cm-opt-out">(opt-out)</span><span class="cm-switch"><div class="slider round active"></div></span></label><div id="service-item-matomo-description"><p class="purposes">Objet: Analytics</p></div></div></li></ul></div></li></ul></div><div class="cm-footer"><div class="cm-footer-buttons"><button type="button" class="cm-btn cm-btn-decline cm-btn-danger cn-decline">Je refuse</button><button type="button" class="cm-btn cm-btn-success cm-btn-info cm-btn-accept">Accepter sélectionné</button><button type="button" class="cm-btn cm-btn-success cm-btn-accept-all">Accepter tout</button></div><p class="cm-powered-by"><a target="_blank" href="https://kiprotect.com/klaro" rel="noopener">Réalisé avec Klaro !</a></p></div></div></div></div></div>
 </body>
 </html>
