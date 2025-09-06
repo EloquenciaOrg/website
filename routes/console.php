48b18feec69e965259ca9b70d5197b6de250d29f
Ethandudu <41381482+ethandudu@users.noreply.github.com>
2025-09-06 20:29:19 +0200

    disabled schedule temporary

--- routes/console.php
+++ routes/console.php
@@ -3,14 +3,14 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+//Artisan::command('inspire', function () {
+//    $this->comment(Inspiring::quote());
+//})->purpose('Display an inspiring quote');
 
 
-Schedule::command('app:hatodb')
-    ->hourly()
-    ->withoutOverlapping()
-    ->runInBackground()
-    ->appendOutputTo(storage_path('logs/hatodb.log'));
-    
+//Schedule::command('app:hatodb')
+//    ->hourly()
+//    ->withoutOverlapping()
+//    ->runInBackground()
+//    ->appendOutputTo(storage_path('logs/hatodb.log'));
+//
